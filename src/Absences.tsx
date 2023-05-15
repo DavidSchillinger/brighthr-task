3632eeb9143c9accc4f5d60a87342aae72d73c08
David Schillinger <david_schillinger@hotmail.com>
2023-05-15 12:40:10 +0100

    feat: allow focussing on individual employee absences

--- src/Absences.tsx
+++ src/Absences.tsx
@@ -1,47 +1,44 @@
-import {Absence, AbsenceType} from '../api/absences'
-import {CSSProperties, Fragment} from 'react'
+import {Absence} from '../api/absences'
+import {CSSProperties, useState} from 'react'
 import {Sorting, useSorting} from './Sorting'
+import {AbsenceCard} from './AbsenceCard'
 
 
-const {format: formatDate} = new Intl.DateTimeFormat('en-GB', {dateStyle: 'short'})
-
-const absenceType: Record<AbsenceType, string> = {
-	FAMILY: 'Family',
-	COMPASSIONATE_LEAVE: 'Compassionate leave',
-	MEDICAL: 'Medical',
-	SICKNESS: 'Sickness',
-	ANNUAL_LEAVE: 'Annual leave',
-}
-
-// TODO: Replace by CSS classes, styled components, Tailwind or similar.
-const absenceStyle: CSSProperties = {
-	display: 'block',
-	marginBottom: '1rem',
+const containerStyle: CSSProperties = {
+	display: 'flex',
+	gap: '2rem',
 }
 
 export function Absences(props: {absences: Absence[]}) {
 	const {absences, setSort} = useSorting(props.absences)
+	const [focussedAbsences, setFocussedAbsences] = useState<Absence[]>([])
 
 	return (
-		<Fragment>
-			<Sorting onChange={setSort}/>
+		<div style={containerStyle}>
+			<div>
+				<Sorting onChange={setSort}/>
 
-			<hr/>
+				<hr/>
 
-			{absences.map(absence => (
-				<section
-					key={absence.id}
-					data-test='absence-card'
-					style={absenceStyle}
-				>
-					Employee: {absence.employee.firstName} {absence.employee.lastName} <br/>
-					Start date: {formatDate(absence.start)} <br/>
-					End date: {formatDate(absence.end)} <br/>
-					Status: {absence.approved ? 'Approved' : 'Pending approval'} <br/>
-					Reason: {absenceType[absence.type]} <br/>
-					Conflict: {absence.hasConflict ? 'Yes' : 'No'} <br/>
-				</section>
-			))}
-		</Fragment>
+				{absences.map(absence => (
+					<AbsenceCard
+						key={absence.id}
+						absence={absence}
+						onClick={() => setFocussedAbsences(absences.filter(a => a.employee.id === absence.employee.id))}
+					/>
+				))}
+			</div>
+
+			{focussedAbsences.length > 0 && (
+				<div>
+					{focussedAbsences.map(absence => (
+						<AbsenceCard
+							key={absence.id}
+							absence={absence}
+						/>
+					))}
+				</div>
+			)}
+		</div>
 	)
 }
